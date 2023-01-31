59d7874e79c88e3b22cee8674791d0c80756a6df
1 <115859190+CNnumber1@users.noreply.github.com>
2023-01-31 20:04:07 +0800

    Add files via upload

--- ValueDemo1.java
+++ ValueDemo1.java
@@ -1,5 +1,11 @@
-public class ValueDemon{
+public class ValueDemo1{
 	public static void main(String[] args){
 		System.out.println("!World!!!");
+		System.out.println(-555);
+		System.out.println(123);
+		System.out.println(1111111);
+		System.out.println("???");
+		System.out.println('?');
+		System.out.println(1.23456);
 	}
 }
