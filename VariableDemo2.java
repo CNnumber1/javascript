de2cd9ae59c5be3faeffedd708cff2e16345d67d
1 <115859190+CNnumber1@users.noreply.github.com>
2023-02-02 18:20:07 +0800

    Add files via upload

---
+++ VariableDemo2.java
@@ -0,0 +1,25 @@
+public class VariableDemo2{
+	public static void main(String[] args){
+		//1.基本用法
+		int a = 10;
+		System.out.println(a);
+		System.out.println(a);
+		System.out.println(a);
+		//2.变量参与计算
+		int b = 20;
+		int c = 20;
+		System.out.println(c + b);
+		//3.修改变量的值
+		a = 30;
+		System.out.println(a);
+		System.out.println("------------------------------------");
+		//定义多个变量
+		int d = 100,e = 200,f = 300;
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		//变量必须要赋值
+		int g = 20;
+		System.out.println(g);
+	}
+}
