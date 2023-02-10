ee30dc2754000da81b6674c2e0a6fa03486f2c55
1 <115859190+CNnumber1@users.noreply.github.com>
2023-02-10 10:21:03 +0800

    Add files via upload

---
+++ VariableDemo3.java
@@ -0,0 +1,11 @@
+public class VariableDemo3{
+	public static void main(String[] args){
+		System.out.println("-------------");
+		int d = 100, e = 200, f = 300;
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		int g = 500;
+		System.out.println(g);
+	}
+}
